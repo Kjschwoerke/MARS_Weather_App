11f9cb2c538919bbda7457e2f4b8faf5365119e6
kjschwoerke <kjschwoerke@yahoo.com>
2020-05-18 19:57:40 -0500

    FetchRequest working and rendering data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'normalize.css'; 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Mars extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      weather: []
+    }
+  }
+
+
+componentDidMount() {
+  let dayArray = []
+
+   //Fetch test data from the NASA Open APIs and display information on the screen:
+fetch('https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0',{
+  mode: 'cors'
+  })
+      .then(response => response.json())
+      .then(data =>  {
+          const dayArr = data.sol_keys
+          const weatherArr = []
+          
+           for (let i=1; i<dayArr.length; i++) {
+             let sol = dayArr[i]
+             weatherArr.push({
+              day: sol, 
+              season: data[sol].Season, 
+              wind: data[sol].HWS.av,
+              direction: data[sol].WD.most_common.compass_point,
+              pressure: data[sol].PRE.av,
+              temperature: data[sol].AT.av
+            })
+          }
+          // console.log(weatherArr)
+          this.setState({
+            weather: weatherArr
+          })
+       })
 }
 
-export default App;
+
+ 
+
+render() {
+const {weather} = this.state    
+// console.log(`This is what is in the render: ${weather}`)
+
+    return (
+      <div className="App">
+        <header>
+          <div>
+            <h1>MARS</h1>
+            <h2>Weather Watch</h2>
+          </div>
+        </header>
+  {/* Create the weather data cards */}
+        <section>
+
+          {weather.map(d => {
+            // console.log(d)
+            return(
+              <article key={d.day}>
+                <h3>Day: SOL {d.day}</h3>
+                  <ul>
+                    <li>Temperature: {d.temperature}</li>
+                    <li>Wind Speed: {d.wind}</li>
+                    <li>Wind Direction: {d.direction}</li>
+                    <li>Season: {d.season}</li>
+                  </ul>
+              </article>
+            )
+          })}
+
+        </section>
+      </div>
+    );
+  }
+}
+  
+
+export default Mars;
