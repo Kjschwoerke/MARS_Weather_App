5c69c8d0df304592fb5fb640871775e9ae2055b5
kjschwoerke <kjschwoerke@yahoo.com>
2020-06-13 18:54:11 -0500

    restructured files and added express server

---
+++ Server/build/precache-manifest.4e90118869f365e460759901d5122e60.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8428fe13741d278a69db896bc89d91d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71c627051525642dc27e",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "5fddc6cff4dfbc607c78",
+    "url": "/static/css/main.ceb57760.chunk.css"
+  },
+  {
+    "revision": "71c627051525642dc27e",
+    "url": "/static/js/2.86ddbd94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.86ddbd94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fddc6cff4dfbc607c78",
+    "url": "/static/js/main.c2c34c0c.chunk.js"
+  },
+  {
+    "revision": "f1d83546b443f65581d0",
+    "url": "/static/js/runtime-main.02714224.js"
+  },
+  {
+    "revision": "1b170bcedf88c49a7d05a95c2e801ebe",
+    "url": "/static/media/Mars.1b170bce.jpg"
+  }
+]);
